75d7a88cc2489caacc9a77d72feda5bedccf0c5f
Cédric LESEC <cedric.lesec@gmail.com>
2025-05-22 14:09:22 +0200

    Add Tailwind CSS support and update website layout
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Updated the main layout in App.vue to include a responsive navigation bar and hero section.
    - Introduced new SVG assets for branding and icons.
    - Removed unused CSS variables and simplified the base CSS file.
    - Created a new MdiIcon component for dynamic icon rendering.

---
+++ src/components/MdiIcon.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import * as AllIcons from '@mdi/js';
+import { computed } from 'vue';
+
+type IconName = keyof typeof AllIcons | string;
+
+interface Props {
+    icon: IconName;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+});
+
+const getIconPath = (iconName: string): string => {
+    return (AllIcons as Record<string, string>)[iconName];
+};
+
+const path = computed(() =>
+    getIconPath(props.icon)
+);
+</script>
+
+<template>
+    <svg fill="currentColor" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg" v-if="path !== null"
+        v-bind="$attrs">
+        <path :d="path"></path>
+    </svg>
+</template>
